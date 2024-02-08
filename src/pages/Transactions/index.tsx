c3e7c026dd5c4df12b241d51ec98a62530a846c4
NicolasLOrtiz <contato.nicolol@gmail.com>
2024-02-08 07:26:56 -0300

    Componente: Summary

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,11 @@
 import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
 
-export function Transactions() {
+export const Transactions = () => {
   return (
     <div>
       <Header />
+      <Summary />
     </div>
   );
 }
