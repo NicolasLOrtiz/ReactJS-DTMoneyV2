cb6fa0372428b78d9825a1b91df851ab94173f75
NicolasLOrtiz <contato.nicolol@gmail.com>
2024-02-27 23:04:40 -0300

    Aplicando o useCallback

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -1,4 +1,4 @@
-import {ReactNode, useEffect, useState} from 'react';
+import {ReactNode, useCallback, useEffect, useState} from 'react';
 import {createContext} from 'use-context-selector'
 import {api} from "../lib/axios.ts";
 
@@ -33,7 +33,7 @@ export const TransactionsContext = createContext({} as TransactionContextType);
 export const TransactionsProvider = ({children}: TransactionsProviderProps) => {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
 
-  async function fetchTransactions(query?: string) {
+  const fetchTransactions = useCallback(async (query?: string) => {
     const response = await api.get('transactions', {
       params: {
         _sort: 'createdAt',
@@ -45,9 +45,9 @@ export const TransactionsProvider = ({children}: TransactionsProviderProps) => {
     setTransactions(response.data)
 
     setTransactions(response.data)
-  }
+  }, [])
 
-  async function createTransaction(data: CreateTransactionInput) {
+  const createTransaction = useCallback(async (data: CreateTransactionInput) => {
     const {description, price, category, type} = data;
 
     const response = await api.post('transactions', {
@@ -59,11 +59,11 @@ export const TransactionsProvider = ({children}: TransactionsProviderProps) => {
     });
 
     setTransactions(state => [response.data, ...state])
-  }
+  }, [])
 
   useEffect(() => {
     fetchTransactions()
-  }, []);
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <TransactionsContext.Provider
